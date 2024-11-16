d92ce4e787e5d52081355c2093c01307dcdb5168
jessica-nguyen-dev <jessica.u.nguyen@sjsu.edu>
2024-11-16 12:22:39 -0800

    Improved instructions in the settings.py file for clarity. I got rid of the environment variable option for simplicities' sake.

--- config/settings.py
+++ config/settings.py
@@ -1,28 +1,19 @@
 """
 Configure settings for the Server Monitoring and Alert System here.
 
-To securely set your email password, use the EMAIL_PASSWORD environment variable:
-    - Windows (Command Prompt):     set EMAIL_PASSWORD=your-email-password
-    - Windows (PowerShell):         $Env:EMAIL_PASSWORD = "your-email-password"
-    - macOS/Linux (Terminal):       export EMAIL_PASSWORD=your-email-password
+IMPORTANT NOTE: If you don't want to use your own email to send the alerts, you can keep mine and just change the
+receiving address! This is an email I made specifically for this project you can use it. If you use your own, generate
+an APP PASSWORD through Google first, and use that instead of your actual password to bypass 2FA.
 
-    For PyCharm Users: add the variable by clicking "more actions" next to the run/debug buttons. Then, click edit and add
-    the variable, making sure to name it "EMAIL_PASSWORD".
-
-If not set, the default value will be used for testing (this is an email I made specifically for this project you can use it!)
-
-Other settings, like thresholds and email details, can be customized below.
+Other settings, like thresholds and email details, can be customized below!
 """
 
-import os
-
-# Email settings - IMPORTANT NOTE:
-# If you don't want to use your own email to send the alerts, you can keep mine and just change the receiving address!
+# Email settings
 
 EMAIL_HOST = "smtp.gmail.com"  # Replace with your email provider's SMTP server
 EMAIL_PORT = 587  # Replace with the SMTP port (usually 587 for TLS)
 EMAIL_USERNAME = "jessica.u.nguyen.dev@gmail.com"
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "caaf wxzp mwap sgqb")    # default password from my mock email
+EMAIL_PASSWORD = "caaf wxzp mwap sgqb"
 EMAIL_RECEIVER = "jessica.u.nguyen.2003@gmail.com"
 
 # Threshold settings (all values are in terms of percent)
